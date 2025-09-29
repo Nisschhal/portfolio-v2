9dcb203995cb6d3599f2b9513ef2072e30a07b3b
Nischal Puri <mrnischalpuri@gmail.com>
2025-09-29 16:07:19 +0545

    metadata and icon added in blog and root layout

--- app/(blogs)/layout.tsx
+++ app/(blogs)/layout.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import { Header } from '@/sections/Header'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Blog | Nischal Puri',
+  description: "Nischal Puri's Blog - Full Stack Developer and Tech Enthusiast",
+  icons: {
+    icon: '../../assets/images/memoji-computer.png',
+  },
+}
 export default async function BlogLayout({
   children,
 }: Readonly<{
